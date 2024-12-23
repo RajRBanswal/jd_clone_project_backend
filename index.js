7de3ac1bf95b01f65859f301b7daa7406a17d3ee
RajRBanswal <rajrbanswal@gmail.com>
2024-12-23 11:43:31 +0530

    modify index.js file

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'))
+
+app.listen(8000,()=>{
+    console.log('Server started on port number 8000');
+    
+})
